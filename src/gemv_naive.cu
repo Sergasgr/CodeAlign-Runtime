019304b643bb792236470fae38574638a1aebf44
Sergio Graciá <sergiogdc2005@gmail.com>
2026-08-21 09:55:18 +0200

    Naive Kernel Finished

--- src/gemv_naive.cu
+++ src/gemv_naive.cu
@@ -2,10 +2,19 @@
 #include <iostream>
 
 __global__ void gemv_naive_kernel(const float* d_mat, const float* d_vec, float* d_out, int rows, int cols) {
-
+    int row = blockIdx.x * blockDim.x + threadIdx.x;
+    if(row < rows) {
+        float sum = 0.0f;
+        for(int i = 0; i < cols; i++) {
+            sum += d_mat[row * cols + i] * d_vec[i]; 
+        }
+        d_out[row] = sum;
+    }
 }
 
 void run_gemv_naive(const float* d_mat, const float* d_vec, float* d_out, int rows, int cols) {
-
+    int block_size = 256;
+    int grid_size = (rows + block_size - 1) / block_size;
+    gemv_naive_kernel<<<grid_size, block_size>>>(d_mat, d_vec, d_out, rows, cols);
+    cudaDeviceSynchronize();
 }
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include "gemv.h"
+#include <cuda_runtime.h>
 #include <iostream>
 #include <vector>
 
-int main() { // thrust::host_vector y thrust::device_vector??
+int main() { // nsight-compute??
     const int N = 5;
-    int rows = 1024, cols = 1024;
+    int rows = 4864, cols = 896;
     size_t bytes_mat = rows * cols * sizeof(float);
     size_t bytes_vec = cols * sizeof(float);
     size_t bytes_out = rows * sizeof(float);
@@ -19,16 +20,37 @@ int main() { // thrust::host_vector y thrust::device_vector??
     cudaMalloc((void**)&d_out, bytes_out);
 
     cudaMemcpy(d_mat, h_mat.data(), bytes_mat, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_vec, h_vec, bytes_vec, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_out, h_out, bytes_out, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_vec, h_vec.data(), bytes_vec, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_out, h_out.data(), bytes_out, cudaMemcpyHostToDevice);
 
-    run_gemv_naive(d_mat, d_vec, d_out, rows, cols);
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
 
-    cudaMemcpy(d_out, h_out.data(), bytes_mat, cudaMemcpyDeviceToHost);
+    float total_ms = 0;
+    const int NUM_ITERATIONS = 100;
 
+    for(int i = 0; i < NUM_ITERATIONS; i++) { 
+        cudaEventRecord(start);
+        run_gemv_naive(d_mat, d_vec, d_out, rows, cols);
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+
+        float milliseconds = 0;
+        cudaEventElapsedTime(&milliseconds, start, stop);
+        if (i >= 10) total_ms += milliseconds;
+    }
+
+    cudaMemcpy(h_out.data(), d_out, bytes_out, cudaMemcpyDeviceToHost);
     for(int i = 0; i < 10; i++) {
         std::cout << h_out[i] << " ";
     }
+    
+    float avg_ms = total_ms / (NUM_ITERATIONS - 10);
+    float avg_bandwidth = ((bytes_mat + bytes_vec + bytes_out) / 1e6) / avg_ms;
+
+    std::cout << "\nAverage Execution time: " << avg_ms << " ms\n";
+    std::cout << "Average Bandwidth " << avg_bandwidth << " GB/s\n";
 
     cudaFree(d_mat);
     cudaFree(d_vec);
